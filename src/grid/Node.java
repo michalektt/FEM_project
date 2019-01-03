590c510fd7e592c7c36547153be2e4ada99f83c8
unknown <tmichal1997@gmail.com>
2019-01-03 18:26:17 +0100

    fem

---
+++ src/grid/Node.java
@@ -0,0 +1,45 @@
+package grid;
+
+import data.ReadTXT;
+
+public class Node {
+
+    private double x, y;
+    private int BC;//1 - na brzegu, 0 - w środku
+
+    private double[] temperature;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature, int i) {
+        this.temperature[i] = temperature;
+    }
+
+    void printNode(){
+        System.out.println("x=" + this.x + " y=" + this.y);
+    }
+
+    public Node(ReadTXT read){
+        this.temperature = new double[(int)(read.getSimulationTime()/read.getStepTime())];
+
+    }
+
+}
